42a72b3b351dc0c8ac6c6d0354f127b907ec601d
sahayogeshwaranp <155245416+SAHAYOGESHWARAN@users.noreply.github.com>
2025-04-08 10:45:05 +0530

    wistom check

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,19 +1,19 @@
-const winston = require('winston'); // Importing winston for logging
+const winston = require('winston'); 
 
 // Configure winston logger
 const logger = winston.createLogger({
-    level: 'info', // Default log level
+    level: 'info',
     format: winston.format.combine(
-        winston.format.timestamp(), // Add timestamp to logs
-        winston.format.json() // Format logs as JSON
+        winston.format.timestamp(), 
+        winston.format.json() 
     ),
     transports: [
-        new winston.transports.Console({ // Log to console
-            format: winston.format.simple(), // Simple format for console
+        new winston.transports.Console({ 
+            format: winston.format.simple(), 
         }),
-        new winston.transports.File({ // Log to a file
-            filename: 'application.log', // Log file name
-            level: 'error', // Only log error level messages to file
+        new winston.transports.File({ 
+            filename: 'application.log', 
+            level: 'error', 
         }),
     ],
 });
@@ -31,7 +31,7 @@ const logMessage = (level, message) => {
             logger.error(message);
             break;
         default:
-            logger.info(message); // Default to info level
+            logger.info(message); 
     }
 };
 
