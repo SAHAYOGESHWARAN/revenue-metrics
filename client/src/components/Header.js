ffd9541823d98f9caf111e86b1c8906ed18477c8
sahayogeshwaranp <155245416+SAHAYOGESHWARAN@users.noreply.github.com>
2025-04-08 10:27:27 +0530

    header update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.css'; 
 
 const Header = () => {
     return (
-        <header>
-            <h1>Revenue Metrics</h1>
+        <header className="header">
+            <div className="logo">
+                <h1>Revenue Metrics</h1>
+            </div>
+            <nav className="nav">
+                <ul>
+                    <li>
+                        <Link to="/" className="nav-link">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about" className="nav-link">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/metrics" className="nav-link">Metrics</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact" className="nav-link">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 };
